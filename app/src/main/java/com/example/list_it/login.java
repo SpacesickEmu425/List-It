7717ab4d2b948f7db6f15594793ced8a56371126
SpacesickEmu425 <moonknigth2@gmail.com>
2021-11-09 19:16:44 -0600

    Initial commit

---
+++ app/src/main/java/com/example/list_it/login.java
@@ -0,0 +1,18 @@
+package com.example.list_it;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void launchsignin(View view) {
+    }
+}
