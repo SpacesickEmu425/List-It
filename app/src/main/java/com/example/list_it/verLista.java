145874f182121ea3c1c86e6703b6b0e97cac0b4e
Yaeldiosdado <ayaeld@hotmail.com>
2021-11-09 20:39:55 -0600

    verLista

--- app/src/main/java/com/example/list_it/verLista.java
+++ app/src/main/java/com/example/list_it/verLista.java
@@ -2,7 +2,9 @@ package com.example.list_it;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class verLista extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class verLista extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ver_lista);
+    }
+
+    public void launchCrearListaLista(View view) {
+    }
+
+    public void launchCrearLista(View view) {
+            Intent crearLista = new Intent (this, crearLista.class);
+            startActivity(crearLista);
     }
 }
