365359637cc75ffce5cdc5db666f415cd1ae0990
Yaeldiosdado <ayaeld@hotmail.com>
2021-11-09 19:36:56 -0600

    Ventanas

---
+++ app/src/main/java/com/example/list_it/verLista.java
@@ -0,0 +1,14 @@
+package com.example.list_it;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class verLista extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ver_lista);
+    }
+}
