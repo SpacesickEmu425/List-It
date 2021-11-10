91c18eef0d2d284917cc8d66e12a0cf950c8e566
Yaeldiosdado <ayaeld@hotmail.com>
2021-11-09 20:08:40 -0600

    MainActivity

--- app/src/main/java/com/example/list_it/MainActivity.java
+++ app/src/main/java/com/example/list_it/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.list_it;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void launchVerLista(View view) {
+        Intent verLista = new Intent (this, verLista.class);
+        startActivity(verLista);
+    }
+
+    public void launchCrearLista(View view) {
+        Intent crearLista = new Intent (this, crearLista.class);
+        startActivity(crearLista);
     }
 }
