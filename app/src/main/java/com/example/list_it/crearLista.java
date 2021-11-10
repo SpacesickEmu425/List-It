57c7a9aeb4ba53034adb63498ab817435b52d26b
Yaeldiosdado <ayaeld@hotmail.com>
2021-11-09 21:09:32 -0600

    crearLista

--- app/src/main/java/com/example/list_it/crearLista.java
+++ app/src/main/java/com/example/list_it/crearLista.java
@@ -2,7 +2,9 @@ package com.example.list_it;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class crearLista extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class crearLista extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_lista);
+    }
+
+    public void launchVerLista(View view) {
+        Intent verLista = new Intent (this, verLista.class);
+        startActivity(verLista);
     }
 }
